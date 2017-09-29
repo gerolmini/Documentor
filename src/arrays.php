901530f6e8bb094cd655dfc98e40692ef04622f4
A4Alumno <a4alumno@aules.local>
2017-09-29 14:24:59 +0200

    comentado de código

--- src/arrays.php
+++ src/arrays.php
@@ -1,61 +1,42 @@
 <?php
 /**
+* Librería de funciones de test.
 *
+* @author Gustavo <Gustavo@mail.com>
 *
 */
 namespace Lib\arrays;
 
 /**
-*
+* @param int $array declaramos cual es la cadena de números que vamos a analizar.
 *
 */
-$array =[3,4,6,1,65,-3,6,10];
-$a = $array;
-
 /**
+* Función que busca el número mayor de un array.
 *
-*
+* @param int $array de números enteros de la cual queremos saber cual es el mayor.
+* @return int $max Devuelve el número mayor del array.
 */
 function max($array){
-  if(count($a)>0){
-    $max=$a[0];
-    for($i=1; $i<count($a); $i++){
-      if($max<$a[$i]){
-        $max = $a[$i];
-      }
-    }
-    return $max;
-  }else{
-    return false;
-  }
+  return 0;
 }
-return 0;
-}
-
 
 /**
+* Función que busque el mínimo de un array de enteros recibido como argumento.
 *
-*
+* @param int $array de números enteros de la cual queremos saber cual es el menor.
+* @return int $max Devuelve el número menor del array.
 */
 function min($array){
-  if(count($a)>0){
-    $min=$a[0];
-    for($i=1; $i<count($a); $i++){
-      if($min>$a[$i]){
-        $min = $a[$i];
-      }
-    }
-    return $min;
-  }else{
-    return false;
-  }
-}
-return 0;
+  return 0;
 }
 
 /**
+* Función que secuencia mayor en un array.
 *
-*
+* Una sección de "espejo" en una matriz es un grupo de elementos contiguos tales que en algún lugar de la matriz, el mismo grupo aparece en el orden inverso.
+* @param int $array variable que recibe la función.
+* @return int Devolver el tamaño de la sección espejo más grande que se encuentra en la matriz dada.
 */
 function espejoMayor($array){
   return 0;
--- src/strings.php
+++ src/strings.php
@@ -1,52 +1,19 @@
- <?php
+<?php
 /**
+* Librería de funciones de test.
 *
-*
+* @author anónimo <anonimo@mail.com>
+* @license GNU
 */
 namespace Lib\strings;
 /**
+* Función que busca una cadena en el medio de un string.
 *
-*
-*/
-$txt="a3d<->l44";
-
-echo $txt;
-echo "<br><br>";
-/**
-*
-*
+* @param string[] $txt string que vamos a recorrer buscando el signo "-" para identificar la cadena.
+* @return int si encuentra el simbolo "-" dependiendo de en que posición de la cadena se encuentra devolvera false or true.
 */
 function flechaMedio($txt){
-
-    if($txt !="") {
-      $i = strlen($txt)/2;
-      $i = intval($i);
-      // encuentra la secuencia de caracteres en la mitad
-      if($txt[$i]=="-") {
-        $cadena = $i;
-        // más de 2 caracteres en la primera mitad
-      }elseif($txt[$i+1]=="-") {
-        $cadena =$i;
-        // más de 2 caracteres en la segunda mitad
-      }
-
-      elseif($txt[$i-1] == "-") {
-        $cadena = $i;
-      }else{
-
-        return false;
-      }
-
-      if($txt[$cadena-1] == "<" && $txt[$cadena+1] == ">"){
-        return true;
-
-      }else{
-        return false;
-
-      }
-    }
-  }
-  echo "el resultado es: " .BuscarCoincidenciaEnUnString($txt);
-
+return 0;
 }
+
 ?>
